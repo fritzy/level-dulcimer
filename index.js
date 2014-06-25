0d00b9edeef44f70dec9069274cde5c1f8cebbba
Nathan Fritz <fritzy@netflint.net>
2014-06-24 21:18:16 -0700

    updated to include level-increment

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var LevelBucket = require('level-bucket');
 var Level2i = require('level-2i');
 var levelup = require('levelup');
+var LevelIncrement = require('level-increment');
 
 function LevelDulcimer(path) {
-    return Level2i(LevelBucket(levelup(path, {valueEncoding: 'json'})));
+    return Level2i(LevelIncrement(LevelBucket(levelup(path, {valueEncoding: 'json'}))));
 }
 
 module.exports = LevelDulcimer;
