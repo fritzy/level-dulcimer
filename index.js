1f491d605218b91f20283308448174cd4c00f42b
Nathan Fritz <fritzy@netflint.net>
2014-07-31 17:16:01 -0700

    added continuation support

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var Level2i = require('level-2i');
 var levelup = require('levelup');
 var LevelIncrement = require('level-increment');
 var LevelBucketArray = require('level-bucketarray');
+var LevelForeignKeys = require('level-foreignkeys');
+var LevelContinuation = require('level-continuation');
 
 function LevelDulcimer(path) {
-    return Level2i(LevelIncrement(LevelBucketArray(LevelBucket(levelup(path, {valueEncoding: 'json'})))));
+    return LevelForeignKeys(Level2i(LevelContinuation(LevelIncrement(LevelBucketArray(LevelBucket(levelup(path, {valueEncoding: 'json'})))))));
 }
 
 module.exports = LevelDulcimer;
