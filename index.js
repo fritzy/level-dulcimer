d218c39601c3a4b6791c99d7a0e3cca67960bdf7
Nathan Fritz <fritzy@netflint.net>
2014-08-06 11:00:01 -0700

    fix to actually use levelout

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var LevelBucket = require('level-bucket');
 var Level2i = require('level-2i');
-var levelup = require('levelup');
+var levelout = require('levelout');
 var LevelIncrement = require('level-increment');
 var LevelBucketArray = require('level-bucketarray');
 var LevelForeignKeys = require('level-foreignkeys');
 var LevelContinuation = require('level-continuation');
 
 function LevelDulcimer(path) {
-    return LevelForeignKeys(Level2i(LevelContinuation(LevelIncrement(LevelBucketArray(LevelBucket(levelup(path, {valueEncoding: 'json'})))))));
+    return LevelForeignKeys(Level2i(LevelContinuation(LevelIncrement(LevelBucketArray(LevelBucket(levelout(path, {valueEncoding: 'json'})))))));
 }
 
 module.exports = LevelDulcimer;
